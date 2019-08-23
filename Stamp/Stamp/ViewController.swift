96cf2b36ce02d8b86bdd0b1c8da610b6eb93e469
Yusuke Hosaka <y-hosaka@mercari.com>
2019-08-23 19:03:47 +0900

    initial commit

---
+++ Stamp/Stamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stamp
+//
+//  Created by y-hosaka on 2019/08/23.
+//  Copyright © 2019 yusuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
